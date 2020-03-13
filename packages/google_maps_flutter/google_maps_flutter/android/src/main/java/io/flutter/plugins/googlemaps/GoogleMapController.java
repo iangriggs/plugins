7d0833fa685787b0e028be747317f391bc66311b
Ian Griggs <iangriggs@hotmail.com>
2020-03-13 10:36:59 +0000

    Added workaround for blank map on resume.

--- packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
+++ packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
@@ -556,6 +556,11 @@ final class GoogleMapController
       return;
     }
     mapView.onResume();
+    // Workaround for https://github.com/flutter/flutter/issues/40284
+    // This apparently forces a re-render of the map.
+    if (googleMap != null) {
+      googleMap.setMapType(googleMap.getMapType());
+    }
   }
 
   @Override
